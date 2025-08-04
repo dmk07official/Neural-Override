4063472fd084ee93946af99d204a38d698d62ec7
dmk07official <122459984+dmk07official@users.noreply.github.com>
2025-08-04 22:45:48 +0200

    Add files via upload

--- animation.js
+++ animation.js
@@ -1,18 +1,20 @@
 const isMobile = window.innerWidth < 600;
+
+const thresholdValue = isMobile ? 0.1 : 0.2;
 const observer = new IntersectionObserver((entries, obs) => {
   entries.forEach(entry => {
-    if (entry.intersectionRatio >= (isMobile ? 0.1 : 0.2)) {
+    if (entry.intersectionRatio >= thresholdValue) {
     entry.target.classList.add('visible');
     obs.unobserve(entry.target);
     }
   });
 }, {
-threshold: isMobile ? 0.05 : 0.2
+  threshold: thresholdValue,
 });
-
 
 window.addEventListener('load', () => {
-const fadeElems = document.querySelectorAll('.fade-in');
-console.log('Fade-in Elemente:', fadeElems.length);
-fadeElems.forEach(el => observer.observe(el));
+  const fadeElems = document.querySelectorAll('.fade-in');
+  console.log('Fade-in Elemente:', fadeElems.length);
+  fadeElems.forEach(el => observer.observe(el));
 });
+
